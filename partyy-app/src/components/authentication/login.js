18e42f7ccaf27f40c412ac272526768052fea926
Jameka Echols <jameka.echols@gmail.com>
2019-05-02 09:56:19 -0500

    added reactstrap

--- partyy-app/src/components/authentication/login.js
+++ partyy-app/src/components/authentication/login.js
@@ -1,2 +1,73 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
+
+class Login extends Component {
+    state = {
+        username: '',
+        password: '',
+        activeUser: sessionStorage.getItem("userId")
+    }
+
+    // this function will handle the values that are being entered in the input fields and set their values to be 
+    // the value of the state 
+    handleFieldChange = event => {
+        const stateToChange = {}
+        stateToChange[event.target.id] = event.target.value
+        this.setState(stateToChange)
+    }
+
+    // this function will do all the verification and checking for the users information matching 
+    // and alerts the user if they entered the wrong information 
+    handleLogin = event => {
+        event.preventDefault()
+
+        // set user to a variable and the .find() will return the user that was found, meaning they are a registerd user
+        let user = this.props.users.find(user => {
+            return user.username === this.state.username && user.password === this.state.password
+        })
+
+        // checks that all the inputs are not empty and or if the the user enters the correct information
+        if (this.state.username === "") {
+
+            alert("Please enter your username!")
+
+        } else if (this.state.password === "") {
+            alert("Please enter your password!")
+        } else if (this.state.username === "" && this.state.password === "") {
+            alert("Please enter your credentials!")
+        } else if (user !== undefined) {
+
+            // if the user is not undefined, then set the sessionStorage.() and set the state
+            this.setState({ activeUser: sessionStorage.setItem("userId", user.id) })
+            console.log(sessionStorage.getItem("userId"))
+            this.props.history.push('/')  // go to to the homepage
+        } else {
+            alert("You've entered the wrong username and/or password")
+        }
+    }
+
+
+    handleSignUp = () => {
+        this.props.history.push('/register') // if the user presses the sign up button, then they should be redirected to the register page
+    }
+
+
+    render() {
+        // clear the session from the previous user
+        sessionStorage.clear()
+
+        return (
+            <React.Fragment>
+                <form>
+                    <input
+                        placeholder="username"
+                        id="username"
+                        required
+                        onChange={this.handleFieldChange}
+                    />
+                </form>
+            </React.Fragment>
+        )
+    }
+}
+export default withRouter(Login)
