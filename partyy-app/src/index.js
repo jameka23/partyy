7d8145f9f6aff784e3506a1e170066eb5e79ddeb
Jameka Echols <jameka.echols@gmail.com>
2019-05-02 13:00:28 -0500

    added bootstrap npm

--- partyy-app/src/index.js
+++ partyy-app/src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Party from './components/Party'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from "react-router-dom"
 import './index.css';
 
